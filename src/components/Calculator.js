b87f5b50a606ae657e0975f51c64b8bfb1965219
Chris Baker <chrisbaker@Chriss-MacBook-Pro.local>
2020-01-27 14:49:39 -0500

    Buttons are working, will produce solutions, still has a ton of bugs and errors, especially subtracting.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -10,31 +10,119 @@ class Calculator extends React.Component {
         super(props)
 
         this.state = {
-            display: "",
-            displayedValue: ""
+            display: "0",
+            currentValue: "", //Can't use display to store current value, it updates async
+            storedValue: "",
+            formulaToEval: "",
+            clearDisplay: false
 
         }
 
-        this.buttonPressed = this.buttonPressed.bind(this);
+        this.handleNumberPressed = this.handleNumberPressed.bind(this);
+        this.handleOperation = this.handleOperation.bind(this);
+        this.handleClear = this.handleClear.bind(this);
+        this.updateDisplay = this.updateDisplay.bind(this);
+        this.handleSolve = this.handleSolve.bind(this)
+
     }
 
-    buttonPressed(event) {
+    /**
+     * This method uses an array to "build" a number,
+     * then stores it as the currentValue
+     * @param {*} event 
+     */
+    handleNumberPressed(event) {
         let value = event.target.value;
-        log(value + " ran");
-        if (value !== undefined) {
-            this.setState({
-                display: value
-            })
-        } else {
-            this.setState({
-                display: undefined
-            })
+        let numArr = this.state.currentValue.split("");
+
+        //Uses regex to prevent number from starting with zero
+        if (/^0+/g.test(this.state.currentValue)) {
+            log("started with zero")
+            numArr.shift();
         }
+
+        numArr.push(value);
+        value = numArr.join("")
+        this.setState({
+            currentValue: value
+        }, () => {
+            this.updateDisplay(this.state.currentValue)
+        })
+
+    }
+
+    /**
+     * Made this separate method in order to force the display to update
+     * in the correct order (was behaving async before this method)
+     * @param {} value 
+     */
+    updateDisplay(value) {
+        this.setState({
+            display: value
+        })
 
     }
 
 
+    handleOperation(event) {
+        let op = event.target.value;
+        let formArr = this.state.formulaToEval.split("")
 
+        //Starts the formula
+        if (formArr.length < 1 && op !== "-") {
+            formArr[0] = this.state.currentValue;
+            formArr.push(op);
+
+        } else { //pushes on the current value if chaining commands
+            formArr.push(this.state.currentValue);
+        }
+        op = formArr.join('')//reseting to string
+        log(op +" op")
+        this.setState({
+            formulaToEval: op,
+            clearDisplay: true
+        })
+        this.setState({
+            currentValue: ""
+        })
+
+
+    }
+
+    handleClear() {
+        this.setState({
+            display: "0",
+            currentValue: "", //Can't use display to store current value, it updates async
+            storedValue: "",
+            formulaToEval: "",
+            clearDisplay: false
+        })
+    }
+
+    handleSolve(event) {
+        let equation = this.state.formulaToEval;
+        equation = equation.split('');
+        equation.push(this.state.currentValue);
+        log(equation)
+
+        if(/[+\-*/]/g.test(equation[equation.length-1]) == true){
+            equation = equation.splice(0, equation.length-2)
+            log("ran regex")
+        } else if(this.state.currentValue === "" && /[+\-*/]/g.test(equation[equation.length-2]) == true){
+            equation = equation.splice(0, equation.length - 3)
+            log("ran empty current value and regex")
+            
+        }
+
+        log(equation + " after regeex")
+        equation = equation.join('');
+        let solution = eval(equation)
+
+        this.setState({
+           display: solution
+        })
+
+    }
 
 
 
@@ -43,32 +131,32 @@ class Calculator extends React.Component {
             <div className="calculator">
                 <Display id="display" value={this.state.display} />
 
-                <NumberButton id="one" value="1" onClick={this.buttonPressed} />
-                <NumberButton id="two" value="2" onClick={this.buttonPressed} />
-                <NumberButton id="three" value="3" onClick={this.buttonPressed} />
+                <NumberButton id="one" value="1" onClick={this.handleNumberPressed} />
+                <NumberButton id="two" value="2" onClick={this.handleNumberPressed} />
+                <NumberButton id="three" value="3" onClick={this.handleNumberPressed} />
 
 
-                <NumberButton id="four" value="4" onClick={this.buttonPressed} />
-                <NumberButton id="five" value="5" onClick={this.buttonPressed} />
-                <NumberButton id="six" value="6" onClick={this.buttonPressed} />
+                <NumberButton id="four" value="4" onClick={this.handleNumberPressed} />
+                <NumberButton id="five" value="5" onClick={this.handleNumberPressed} />
+                <NumberButton id="six" value="6" onClick={this.handleNumberPressed} />
 
 
-                <NumberButton id="seven" value="7" onClick={this.buttonPressed} />
-                <NumberButton id="eight" value="8" onClick={this.buttonPressed} />
-                <NumberButton id="nine" value="9" onClick={this.buttonPressed} />
+                <NumberButton id="seven" value="7" onClick={this.handleNumberPressed} />
+                <NumberButton id="eight" value="8" onClick={this.handleNumberPressed} />
+                <NumberButton id="nine" value="9" onClick={this.handleNumberPressed} />
 
-                <NumberButton id="zero" value="0" onClick={this.buttonPressed} />
+                <NumberButton id="zero" value="0" onClick={this.handleNumberPressed} />
 
-                <NumberButton id="decimal" value="." onClick={this.buttonPressed}  />
+                <NumberButton id="decimal" value="." onClick={this.handleNumberPressed} />
 
-                <NumberButton id="plus" value="+" onClick={this.buttonPressed} />
-                <NumberButton id="minus" value="-" onClick={this.buttonPressed} />
-                <NumberButton id="multiply" value="X" onClick={this.buttonPressed} />
-                <NumberButton id="divide" value="/" onClick={this.buttonPressed} />
+                <NumberButton id="add" value="+" onClick={this.handleOperation} />
+                <NumberButton id="subtract" value="-" onClick={this.handleOperation} />
+                <NumberButton id="multiply" value="*" onClick={this.handleOperation} />
+                <NumberButton id="divide" value="/" onClick={this.handleOperation} />
 
-                <NumberButton id="clear" value="AC" onClick={this.buttonPressed} />
+                <NumberButton id="clear" value="AC" onClick={this.handleClear} />
 
-                <NumberButton id="equals" value="=" onClick={this.buttonPressed} />
+                <NumberButton id="equals" value="=" onClick={this.handleSolve} />
 
 
 
--- src/components/Display.js
+++ src/components/Display.js
@@ -6,7 +6,6 @@ const Display = (props) => {
     return(
         <div id={props.id}>
         {props.value}
-        1 2 3 4 5 6 7 8 9 10
 
         </div>
     )
