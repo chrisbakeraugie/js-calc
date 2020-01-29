732c7237a5788828b31c424fcddac10ea27416e7
Chris Baker <chrisbaker@Chriss-MacBook-Pro.local>
2020-01-29 15:59:12 -0500

    Updates

---
+++ precache-manifest.4e0ddba78fda2d228305aaff225ca350.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69fb7d1fba4ef9a5b205a2c0d79f5425",
+    "url": "/js-calc/index.html"
+  },
+  {
+    "revision": "6e02e7745a2a6489f606",
+    "url": "/js-calc/static/css/main.f6f1797d.chunk.css"
+  },
+  {
+    "revision": "a6c233de93a4f02aa8d1",
+    "url": "/js-calc/static/js/2.c8d1f11e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/js-calc/static/js/2.c8d1f11e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e02e7745a2a6489f606",
+    "url": "/js-calc/static/js/main.a9d81c8b.chunk.js"
+  },
+  {
+    "revision": "2be91f191e74d3a6debd",
+    "url": "/js-calc/static/js/runtime-main.4e2f2a3b.js"
+  }
+]);
